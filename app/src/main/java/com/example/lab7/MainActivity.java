834c739088a075d68ba15866684d8438f3527a85
patilkm <117905121+patilkm@users.noreply.github.com>
2023-05-19 11:46:39 +0530

    .

---
+++ app/src/main/java/com/example/lab7/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.lab7;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MainActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
+    String [] arr = {"Java", "Python", "C", "C++", "OOP", "Data Science", "SQL", "AI-ML", "IOT"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        CustomAdapter c = new CustomAdapter(arr);
+        recyclerView.setAdapter(c);
+    }
+}
